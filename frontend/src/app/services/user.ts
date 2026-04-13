fec45b4cf0b8d24e33ca8402683fc72f2475bb7a
Nikolas5516 <gavazn.nikolas@gmail.com>
2026-04-13 20:52:24 +0300

    feat: implement Angular device and user services with HttpClient

--- frontend/src/app/services/user.ts
+++ frontend/src/app/services/user.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class User {}
+export class UserService {
+  private apiUrl = `${environment.apiUrl}/users`;
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
+}
